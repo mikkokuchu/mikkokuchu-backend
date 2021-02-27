290388701169d36b5a4683f76852f320c3dadbb8
kokoa0429 <kokoa@ciebus.net>
2021-02-28 01:16:58 +0900

    Add api exclude

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -6,6 +6,7 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { MikkokuTransaction } from './mikkokuTransaction.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
@@ -14,27 +15,33 @@ export class User {
   id: number;
 
   @Column()
+  @Exclude()
   userStatus: 'pending' | 'authed';
 
   @Column({ default: '' })
+  @Exclude()
   lineId: string;
 
   @Column({ default: '' })
   userName: string;
 
   @Column()
+  @Exclude()
   pendingCommand: '' | 'mikkoku' | 'mission';
 
   @Column()
   lifeCount: number;
 
   @Column({ default: false })
+  @Exclude()
   testAccount: boolean;
 
   @Column({ default: '' })
+  @Exclude()
   code: string;
 
   @Column()
+  @Exclude()
   joinTime: number;
 
   @OneToMany((type) => MikkokuTransaction, (object) => object.sender)
