f4d3736df6de72a5eae4834a80d6f04adda1c065
kokoa0429 <kokoa@ciebus.net>
2021-02-28 07:19:00 +0900

    Add sendmission function

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { MikkokuTransaction } from './entity/mikkokuTransaction.entity';
 import axios from 'axios';
 import { lineToken } from './line';
+import { Mission } from './entity/mission.entity';
 
 @Injectable()
 export class AppService {
@@ -13,6 +14,8 @@ export class AppService {
     private userRepository: Repository<User>,
     @InjectRepository(MikkokuTransaction)
     private mikkokuTransactionRepository: Repository<MikkokuTransaction>,
+    @InjectRepository(Mission)
+    private missionRepository: Repository<Mission>,
   ) {}
 
   getUser(lineId: string): Promise<User> {
@@ -103,6 +106,27 @@ export class AppService {
     }
   }
 
+  async replyMissions(token: string) {
+    const missions = await this.missionRepository.find();
+    const replyData = {
+      type: 'carousel',
+      contents: [],
+    };
+
+    missions.forEach((mission) => {
+      console.log(mission.title);
+      const childItem = JSON.parse(JSON.stringify(BubbleTemplate));
+      childItem.body.contents[0].text = mission.title;
+      childItem.body.contents[1].text = mission.description;
+      childItem.body.contents[2].text = mission.penaltyText;
+      console.log(childItem.footer.contents[0].action.text);
+      childItem.footer.contents[0].action.text = '>q' + mission.id;
+      //console.log(childItem.body.contents[0].action.text);
+      replyData.contents.push(childItem);
+    });
+    await this.replyFlexMessage(token, replyData);
+  }
+
   async replyMessage(token: string, message: string) {
     const replydata = {
       replyToken: token,
@@ -120,6 +144,31 @@ export class AppService {
         Authorization: `Bearer ${lineToken}`,
       },
     });
+  }
+
+  async replyFlexMessage(token: string, data: object) {
+    const replydata = {
+      replyToken: token,
+      messages: [
+        {
+          type: 'flex',
+          altText: 'This is a Flex Message',
+          contents: data,
+        },
+      ],
+    };
+    // よくない
+    const result = await axios.post(
+      'https://api.line.me/v2/bot/message/reply',
+      replydata,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${lineToken}`,
+        },
+      },
+    );
+    console.log(result);
   }
 
   async sendMessage(to: string, message: string) {
@@ -141,3 +190,50 @@ export class AppService {
     });
   }
 }
+
+const BubbleTemplate = {
+  type: 'bubble',
+  body: {
+    type: 'box',
+    layout: 'vertical',
+    contents: [
+      {
+        type: 'text',
+        weight: 'bold',
+        size: 'xl',
+        text: 'ミッション①',
+      },
+      {
+        type: 'text',
+        text: 'test text',
+        wrap: true,
+      },
+      {
+        type: 'text',
+        text: 'test penalty text',
+      },
+    ],
+  },
+  footer: {
+    type: 'box',
+    layout: 'vertical',
+    spacing: 'sm',
+    contents: [
+      {
+        type: 'button',
+        style: 'link',
+        height: 'sm',
+        action: {
+          type: 'message',
+          label: 'このクエストを回答する',
+          text: '>qtest',
+        },
+      },
+      {
+        type: 'spacer',
+        size: 'sm',
+      },
+    ],
+    flex: 0,
+  },
+};
