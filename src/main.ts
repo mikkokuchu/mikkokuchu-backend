6d6769be20dea65404d42c45c690e8d76e51f955
kokoa0429 <kokoa@ciebus.net>
2021-02-28 07:19:08 +0900

    Set CORS

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,20 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
+    res.header(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept,Referer,User-Agent',
+    );
+    res.header('Access-Control-Allow-Credentials', true);
+    if ('OPTIONS' == req.method) {
+      res.send(204); // 204: No Content
+    } else {
+      next();
+    }
   });
   await app.listen(80);
 }
+
 bootstrap();
